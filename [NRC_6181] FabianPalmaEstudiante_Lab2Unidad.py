585bee7427524582c3df35bd9e9b083dba1a50bf
Fabian Palma <fapalma1@espe.edu.ec>
2022-05-15 21:56:18 -0500

    Primera versión Python

---
+++ [NRC_6181] FabianPalmaEstudiante_Lab2Unidad.py
@@ -0,0 +1,22 @@
+
+#encabezado del programa a realizar (presentación)
+#introducción al programa
+def encabezado ():
+    print("Ejercicio - Laboratorio 2 - Unidad 1.")
+    print("Fabián Alexander Palma Dueñas.")
+    print("Programación Orientada a Objetos [NRC 6181].")
+    print("Problelma: Hallar la potencia de cualquier número x^y.")
+#encabezado(nombre)
+#funcion para resolver el programa asignado
+def ejercicioPotencia(numero,numeroPotencia):
+    #realizamos el respectivo proceso para encontrar la potencia de un número
+    resultadoPotencia=numero**numeroPotencia
+    #mostramos los datos ingresados tanto como el respectivo resultado
+    print("El resultado es: ",numero,"^",numeroPotencia, "=", resultadoPotencia)
+    return resultadoPotencia
+#llamado respectivo a cada una de las funciones para ejecutarlas
+encabezado()
+#pedir datos al usuario para resolver el problema
+x=int(input("\nIngrese el número que desea elevar a una potencia: "))
+y=int(input("Ingrese la potencia:  "))
+ejercicioPotencia(x,y)
